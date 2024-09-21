2f6f067073504e610065aae7682a830a6f9aa15f
Reneechang17 <minnie90061717@gmail.com>
2024-09-21 15:38:01 -0400

    write, read and delete the file

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -98,9 +98,9 @@ func (t * TCPTransport) handleConn(conn net.Conn) {
 	// Read Loop
 	rpc := RPC{}
 	for {
-		if err := t.Decoder.Decode(conn, &rpc); err != nil {
-			fmt.Printf("TCP error: %s\n", err)
-			continue
+		err = t.Decoder.Decode(conn, &rpc) 
+		if err != nil {
+			return 
 		}
 		rpc.From = conn.RemoteAddr()
 		t.rpcch <- rpc
---
+++ store_test.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"io"
+	"testing"
+)
+
+func TestPathTransformFunc(t *testing.T) {
+	key := "somemycoursework"
+	pathKey := CASPathTransformFunc(key)
+	// Test the pathname
+	expectesOriginalKey := "f979e8bf9ff685078fa7bf471347a1dc8b5de691"
+	expectedPathName := "f979e/8bf9f/f6850/78fa7/bf471/347a1/dc8b5/de691"
+	if pathKey.PathName != expectedPathName {
+		t.Errorf("Have %s, want %s", pathKey.PathName, expectedPathName)
+	}
+
+	if pathKey.Filename != expectesOriginalKey {
+		t.Errorf("Have %s, want %s", pathKey.Filename, expectesOriginalKey)
+	}
+}
+
+func TestStoreDeleteKey(t *testing.T) {
+	opts := StoreOpts {
+		PathTransformFunc: CASPathTransformFunc,
+	}
+	s := NewStore(opts)
+	key := "somefile"
+	data := []byte("some data")
+
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	if err := s.Delete(key); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestStore(t *testing.T) {
+	opts := StoreOpts {
+		PathTransformFunc: CASPathTransformFunc,
+	}
+	s := NewStore(opts)
+	key := "somefile"
+	data := []byte("some data")
+
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	r, err := s.Read(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := io.ReadAll(r)
+	if string(b) != string(data) {
+		t.Errorf("want %s have %s", data, b)
+	}
+	s.Delete(key)
+}
